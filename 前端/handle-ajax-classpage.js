6c73ca71244af7cad99502503d6ba8dc1d306aeb
Pepper <pepperjao@qq.com>
2019-07-17 16:36:27 +0800

    班级管理页面js

--- 前端/handle-ajax-classpage.js
+++ 前端/handle-ajax-classpage.js
@@ -1,7 +1,7 @@
 var data = [{
     "class_name": "01",
     "teacher": "teacher_a",
-    "students": [1001, 1002, 1003]
+    "students": [1001, 1002, 1003, 244]
     },
     {
     "class_name": "02",
@@ -16,10 +16,12 @@ var data = [{
 
 
 
-for (var i = 0; i < data.length; i++) {//循环json对象，拼接tr,td的html
+for (var i = 0; i < data.length; i++) {//将班级名称加入列表中
     var html = '	<div class="col-md-4"> <!-- Widget: user widget style 1 --><div class="box box-widget widget-user-2">    <!-- Add the bg color to the header using any of the bg-* classes -->    <div class="widget-user-header bg-blue">        <!--<div class="widget-user-image">            <img class="img-circle" src="../dist/img/user7-128x128.jpg" alt="User Avatar">        </div>-->        <!-- /.widget-user-image -->        <h5 class="widget-user-username class_name_tag">';
     html = html + data[i].class_name + '班';
-    html = html + '</h5>   </div>   <div class="box-footer no-padding">        <ul class="nav nav-stacked">            <li><a href="#">学生人数 <span class="pull-right badge bg-blue">31</span></a></li>            <li><a href="#">负责老师 <span class="pull-right badge bg-aqua">李老师</span></a></li>            <!--<li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a></li>            <li><a href="#">Followers <span class="pull-right badge bg-red">842</span></a></li>-->        </ul>    </div></div><!-- /.widget-user --></div>';
+    html = html + '</h5>   </div>   <div class="box-footer no-padding">        <ul class="nav nav-stacked">            <li><a href="#">学生人数 <span class="pull-right badge bg-blue">';
+    html = html + data[i].students.length + '</span></a></li>            <li><a href="#">负责老师 <span class="pull-right badge bg-aqua">';
+    html = html + data[i].teacher + '李老师</span></a></li>            <!--<li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a></li>            <li><a href="#">Followers <span class="pull-right badge bg-red">842</span></a></li>-->        </ul>    </div></div><!-- /.widget-user --></div>';
     $('#class_name_tag').append(html);
     html = ''
 }
