6f3b5130e79f5fa337043437e56260d9b0914685
tylzh97 <tylzh97@126.com>
2019-07-18 21:43:16 +0800

    更新了主网站静态路由设置
    创建了API应用，将路由指向其中。同时，添加了静态资源配置以及媒体文件配置。

--- 后端/TMS/TMS/urls.py
+++ 后端/TMS/TMS/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path("api/", include("api.urls"))
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    # 访问文档文件
+    re_path(r"file/(?P<path>.*)", serve, {"document_root": settings.FILE_ROOT}),
+
+    # 配置静态资源,可以直接通过url访问static目录下的文件,需要放置在最后一条路由规则
+    re_path(r"(?P<path>.*)", serve, {"document_root": settings.PUBLIC_ROOT}),
 ]
