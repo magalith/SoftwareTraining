756df31cf52e89f64a166ed492ca7857349a8542
maglith <942399293@qq.com>
2019-07-19 09:03:15 +0800

    更新教师界面
    更新教师界面

--- 前端/project_js/handle-ajax-mission.js
+++ 前端/project_js/handle-ajax-mission.js
@@ -4,33 +4,34 @@ var data =
             "id": "M_001",
             "text": "作业一",
             "file": "http://0.0.0.0/a.txt", 
-            "deadline": "截止日期:19.19273.2 作业要求"
+            "deadline": "截止日期:19.19273.2"
         }, 
         {
             "id": "M_002",
             "text": "作业二",
             "file": "http://0.0.0.0/b.txt", 
-            "deadline": "截止日期:19.19273.2 作业要求"
+            "deadline": "截止日期:19.19273.2"
         }, 
         {
             "id": "M_002",
             "text": "作业二",
             "file": "http://0.0.0.0/b.txt", 
-            "deadline": "截止日期:19.19273.2 作业要求"
+            "deadline": "截止日期:19.19273.2"
         }, 
         {
             "id": "M_002",
             "text": "作业二",
             "file": "http://0.0.0.0/b.txt", 
-            "deadline": 1563346789
+            "deadline": "截止日期:19.19273.2"
         }
 ]
 
 
 var html = '';
 for ( var i = 0; i < data.length; i++) {
-    html = html + '<li style="font-size: 20px">' + data[i].text +'</li>';
-    html = html + '<li>' + data[i].deadline + '</li> <br/>';
+    html += '<li style="font-size: 20px"><a href="teacher_file_check.html">' + data[i].text +'</a></li>';
+    html += '<li>' + data[i].deadline + '</li> <br/>';
+    html += ''
     $('.missionlist').eq(i).append(html);
     html = ''
 }
