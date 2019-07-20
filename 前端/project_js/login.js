5d8e083e42cdd4e5baa08c5bb3f1d2c5d81d3172
Pepper <pepperjao@qq.com>
2019-07-20 15:13:05 +0800

    update login js

--- 前端/project_js/login.js
+++ 前端/project_js/login.js
@@ -14,9 +14,17 @@ function log_in() {
 
     if(username != "" && username != null && password != "" && password != null){
         // 执行post指令，向服务器传递用户账号
+        $.post("test.php", { "user_name": username, "password":password, "timestamp":timestamp } )
+        // function(data){
+        //   alert(data.name); // John
+        //   console.log(data.time); //  2pm
+        
         console.log(username);
         console.log(password);
     }else {
         alert("请填写完整")
     }
 }
+
+
+
