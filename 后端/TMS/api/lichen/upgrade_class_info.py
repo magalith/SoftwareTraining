e32b89676b9246f42bbf9c3644d56add77603bd5
lichen' job <45004953+lichenrr@users.noreply.github.com>
2019-07-21 11:25:11 +0800

    api

---
+++ 后端/TMS/api/lichen/upgrade_class_info.py
@@ -0,0 +1,18 @@
+from database.models import User, Class
+
+# 管理员接口，更新班级信息
+def upgrade_class(dir_post):
+
+    for i in dir_post:
+        # 更新class.name  class.room
+        i['class_name']
+        i['room']
+        # 更新教室班级id
+        for k in i['teacher']:
+
+        # 更新学生班级id
+        for j in i['students']:
+
+
+
+
