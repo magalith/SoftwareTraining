b301934cc362e2342c707d984ae18ca555af9427
Pepper <pepperjao@qq.com>
2019-07-21 15:41:20 +0800

    Update handle-ajax-student_score.js

--- 后端/TMS/public/project_js/handle-ajax-student_score.js
+++ 后端/TMS/public/project_js/handle-ajax-student_score.js
@@ -3,8 +3,7 @@ $(function(){
     $.post("/api/get_docs", {'timestamp': 1}, function(data){
         loadDocumentList(data);
     }, "json")
-    var file_submit= new FormData($("#file_submit")[0]);
-    $.post("/api/push_doc", {"timestamp":1, "mission_id":"mi_01", "text":"", "file":file_submit} )
+
 })
 
 function loadDocumentList(data) {
@@ -16,7 +15,7 @@ function loadDocumentList(data) {
             html += '<td>' + data[i].id + '</td>';
             html += '<td>' + data[i].text + '</td>';
             html += '<td><input type="file" name="file" id="file_submit"></input></td>';
-            html += '<td><button type="submit" class="btn btn-primary btn-flat btn-sm" id="submit_file">submit</button></td>';
+            html += '<td><button type="submit" class="btn btn-primary btn-flat btn-sm" id="submit_file" onclick="file_submit">submit</button></td>';
             html += '</tr>';
             $('#check_untask').append(html)
             html = ''
@@ -38,30 +37,13 @@ function deleteItem(e){
 }
 
 
- var file_submit= new FormData($("#file_submit")[0]);
-// function submit_file(){
-//     $.post("/api/push_doc",
-//     {"timestamp":1, "mission_id":"mi_01", "text":"", "file":file_submit} )
-    
+function file_submit() {
+    var file=$("#submit_file").files()
+    $.post("/api/push_doc", {"timestamp": 1, "mission_id": "mi_01", "text": "", "file": file})
+    // console.log($(this).index())
+
+}
 
 
-//     $.ajax({
-//         url:'/api/check_mission', /*接口域名地址*/
-//         type:'post',
-//         data: formdata,
-//         processData: false,
-//         contentType : false,
-//         success: function () {
-//         },
-//         error: function () {}
-// }
-// }
 
-function postData(){
-    var formData = new FormData();
-    formData.append("photo",$("#photo")[0].files[0]);
-    formData.append("service",'App.Passion.UploadFile');
-    formData.append("token",token);
-    
-        }
 
