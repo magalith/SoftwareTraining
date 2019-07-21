b6f4ff8c20f8c432bef8e7832662fce79b59fa7a
maglith <942399293@qq.com>
2019-07-21 16:46:50 +0800

    更新界面
    更新界面

--- 后端/TMS/public/project_js/handle-ajax-tch_add_task.js
+++ 后端/TMS/public/project_js/handle-ajax-tch_add_task.js
@@ -1,76 +1,55 @@
-const data =
-[
-        {
-            "id": "M_001",
-            "text": "作业一",
-            "file": "http://0.0.0.0/a.txt", 
-            "deadline": "截止日期:19.19273.2"
-        }, 
-        {
-            "id": "M_002",
-            "text": "作业二",
-            "file": "http://0.0.0.0/b.txt", 
-            "deadline": "截止日期:19.19273.2"
-        }, 
-        {
-            "id": "M_002",
-            "text": "作业二",
-            "file": "http://0.0.0.0/b.txt", 
-            "deadline": "截止日期:19.19273.2"
-        }, 
-        {
-            "id": "M_002",
-            "text": "作业二",
-            "file": "http://0.0.0.0/b.txt", 
-            "deadline": "截止日期:19.19273.2"
-        }
-]
-
-
 
 $(function(){
-    load_missionlist(data);
+    $.post("/api/get_stage", {"timestamp": 1}, function(data){
+        load_missionlist(data)
+    }, "json")
     $('.addlist').on("click", addtext_to_list)
 })
 
 
 function load_missionlist(data){
-    var html = '';
-    for ( var i = 0; i < data.length; i++) {
-        html += '<li style="font-size: 20px"><a href="/tch_check_studoc"><h3>' + data[i].text +'</h3></a></li>';
-        html += '<li>' + data[i].deadline + '</li> <br/>';
-        html += ''
-        $('.missionlist').eq(i).append(html);
-        html = ''
-}
+    data = data.data;
+    for (var i=0; i<data.length; i++){
+        console.log(data[i].missions)
+        if (data[i].missions.length !== 0) {
+            for (var j=0; j<data[i].missions.length; j++) {
+                task_name = data[i].missions[j].text.split("\n")[0];
+                task_detail = data[i].missions[j].text.split("\n")[1];
+                task_ddl = data[i].missions[j].deadline;
+                var html = '';
+                html += '<li style="font-size: 20px"><a href="/tch_check_studoc"><h3>任务名称：' + task_name + '</h3></a></li>';
+                html += '<li>详细描述：' + task_detail + '</li>';
+                html += '<li>截止日期：' + task_ddl + '</li>';
+                $('.missionlist').eq(data[i].stage_number).append(html)
+            }
+        }
+    }
 }
 
 
 function addtext_to_list(){
-    // var tit = $(this).parent().prev().find("input").eq(0).val();
-    console.log($(this).index())
-    var title = $('.title').val();
-    var discription = $('.discription').val();
-    if(title != "" && title != null && discription != "" && discription != null )
-    {
-        // console.log(tit);
-        html = '';
-        html += '<li style="font-size: 20px"><a href="teacher_file_check.html"><h3>' + title +'</h3></a></li>';
-        html += '<li>' + discription + '</li> <br/>';
-
-        $(this).parent().parent().parent().parent().prev().find(".missionlist").append(html)
-        // $('.missionlist').append(html);
+    stage_id = $('.addlist').index(this);
+    console.log(stage_id)
+    var title = $('.title').eq(stage_id).val();
+    var discription = $('.discription').eq(stage_id).val();
+    var deadline = $('.deadline').eq(stage_id).val();
+    if(title != "" && title != null && discription != "" && discription != null && deadline != "" && deadline != null) {
+        data = {"timestamp": 1, "stage_id": stage_id, "mission": '{"text": title+discription, file: "", deadline: "2019-7-30"}'}
+        $.post("/api/push_mission", data, function(data){
+            console.log(data.data);
+        })
+        var html = '';
+        html += '<li style="font-size: 20px"><a href="teacher_file_check.html"><h3>任务名称：' + title +'</h3></a></li>';
+        html += '<li>详细描述：' + discription + '</li>';
+        html += '<li>截止日期：' + deadline + '</li>';
+        $('.missionlist').eq(stage_id).append(html)
         $('.title').val("");
         $('.discription').val("");
-
-    } 
-    else 
-    {
+        $('.deadline').val("");
+    } else{
         alert("请输入完整添加内容")
         $('.title').val("")
         $('.discription').val("")
-
+        $('.deadline').val("");
     }
-
-
 }
