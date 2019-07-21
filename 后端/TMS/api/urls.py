295d89598774945bfbaf749e20e5615df16eab9c
LiZhenghao <tylzh1997@vip.qq.com>
2019-07-21 09:00:24 +0800

    Merge @lichenrr and @Lizhenghao

--- 后端/TMS/api/urls.py
+++ 后端/TMS/api/urls.py
@@ -4,6 +4,7 @@
 from django.urls import path, re_path
 from . import views
 
+
 urlpatterns = [
     #############
     #  通用接口  #
@@ -11,49 +12,49 @@ urlpatterns = [
     # api版本界面,用于测试服务是否可用
     path("", views.api_index),
     # 测试界面,用于测试服务是否可用
-    re_path("test/?", views.test),
+    re_path("test/?$", views.test),
     # 查看自己的信息
-    re_path("get_self_info/?", views.test),
+    re_path("get_self_info/?$", views.test),
 
     ############
     # 管理员接口 #
     ############
     # 管理员查看全部学生名单
-    re_path("get_students/?", views.get_students_list),
+    re_path("get_students/?$", views.get_students_list),
     # 管理员更新班级信息
-    re_path("set_classes/?", views.test),
+    re_path("set_classes/?$", views.test),
     # 管理员获取教师名单
-    re_path("get_teachers/?", views.test),
+    re_path("get_teachers/?$", views.test),
     # 管理员获取所有班级名单(带学生老师名单)
-    re_path("get_class_info/?", views.test),
+    re_path("get_class_info/?$", views.test),
 
     #############
     #  教师接口  #
     #############
     # 查看负责班级的学生名单
-    # re_path("get_classinfo/?", views.test),
+    # re_path("get_classinfo/?$", views.test),
     # 查看所有阶段
-    re_path("get_stage/?", views.test),
+    re_path("get_stage/?$", views.test),
     # 为阶段添加任务
-    re_path("push_mission/?", views.test),
+    re_path("push_mission/?$", views.test),
     # 查看某一任务的所有文档
-    re_path("check_mission/?", views.test),
+    re_path("check_mission/?$", views.test),
     # 更新文档(学生的作业)分数
-    re_path("set_score/?", views.test),
+    re_path("set_score/?$", views.test),
     # 获取某一个学生的所有文档(作业)信息
-    re_path("get_students_doc/?", views.test),
+    re_path("get_students_doc/?$", views.get_all_student_docs),
 
     #############
     #  学生接口  #
     #############
     # 查看所有可选项目
-    re_path("get_project_pool/?", views.test),
+    re_path("get_project_pool/?$", views.get_all_project),
     # 更新学生选择的项目
-    re_path("set_studentproj/?", views.test),
+    re_path("set_studentproj/?$", views.update_student_project),
     # 查看所有的任务(包含已完成任务与未完成任务)
-    re_path("get_missions/?", views.test),
+    re_path("get_missions/?$", views.get_all_mission),
     # 为某个任务提交文档(仅包含附件,文字留空)
-    re_path("push_doc/?", views.test),
+    re_path("push_doc/?$", views.update_doc),
     # 获取所有的文档信息(包含得分)
-    re_path("get_docs/?", views.test),
+    re_path("get_docs/?$", views.get_docs),
 ]
--- 后端/TMS/api/views.py
+++ 后端/TMS/api/views.py
@@ -29,6 +29,13 @@ def test(request):
     }
     return HttpResponse(json.dumps(ans))
 
+
+###########################################################################
+########################       @Licunhao       ############################
+###########################################################################
+
+
+# 管理员获取所有学生列表
 def get_students_list(request):
     data = get_students()
     if request.method == "POST":
@@ -37,3 +44,86 @@ def get_students_list(request):
             "data": data
         }
     return HttpResponse(json.dumps(ans))
+
+
+###########################################################################
+########################     @Lizhenghao       ############################
+########################       Data API        ############################
+########################  2019-07-20 20:20:04  ############################
+###########################################################################
+
+
+# 获取所有文档api
+def get_docs(request):
+    if request.method == "POST":
+        try:
+            uid = request.session.get("uid")
+            if not uid:
+                return HttpResponse(json.dumps(lzh_api.error_with_code(1001), ensure_ascii=False))
+            ans = lzh_api.get_all_doc(uid)
+            # 正确的返回结果
+            return HttpResponse(json.dumps(ans, ensure_ascii=False))
+        except Exception as e:
+            # 若执行过程报错,则返回通用错误.
+            print(e)
+            return HttpResponse(json.dumps(lzh_api.error_with_code(0), ensure_ascii=False))
+    else:
+        # GET方法请求本接口,服务拒绝.
+        return HttpResponse(json.dumps(lzh_api.error_with_code(2004), ensure_ascii=False))
+
+
+# 为指定任务提交文档
+# TODO 未检查数据合法性
+# 暂时未检查所属任务是否为指导老师所出,这个问题可能会造成数据泄露或不规范
+# 若有时间,则需要对此问题进行修复
+# TODO 非常紧急,返回值字段缺失
+# 返回结果中,应当添加字段,表示自己提交的文稿信息.但是返回结果中没有包含该字段
+def update_doc(request):
+    if request.method == "POST":
+        try:
+            uid = request.session.get("uid")
+            if not uid:
+                return HttpResponse(json.dumps(lzh_api.error_with_code(1001), ensure_ascii=False))
+            timestamp = int(request.POST.get("timestamp"))
+            mid = int(request.POST.get("mission_id"))
+            text = request.POST.get("text")
+            file = request.FILES.get("file")
+            ans = lzh_api.add_doc_for_mission(uid=uid, mid=mid, file=file, text=text)
+            # 正确的返回结果
+            return HttpResponse(json.dumps(ans, ensure_ascii=False))
+        except Exception as e:
+            # 若执行过程报错,则返回通用错误.
+            print(e)
+            return HttpResponse(json.dumps(lzh_api.error_with_code(0), ensure_ascii=False))
+    else:
+        # GET方法访问,拒绝请求.
+        return HttpResponse(json.dumps(lzh_api.error_with_code(2004), ensure_ascii=False))
+
+
+# 查看指导教师布置的所有任务信息
+def get_all_mission(request):
+    ans = lzh_api.get_all_mission_status(int(request.session.get("uid")))
+    return HttpResponse(json.dumps(ans, ensure_ascii=False))
+
+
+# 更新学生所选的项目信息
+def update_student_project(request):
+    timestamp = request.POST.get("timestamp")
+    pid = int(request.POST.get("proj_id"))
+    ans = lzh_api.update_student_project(int(request.session.get("uid")), pid)
+    return HttpResponse(json.dumps(ans, ensure_ascii=False))
+
+
+# 查看所有可选项目
+def get_all_project(request):
+    timestamp = request.POST.get("timestamp")
+    ans = lzh_api.check_all_project()
+    return HttpResponse(json.dumps(ans, ensure_ascii=False))
+
+
+# 教师查看某一学生提交的所有文档
+def get_all_student_docs(request):
+    timestamp = request.POST.get("timestamp")
+    sid = int(request.POST.get("s_id"))
+    ans = lzh_api.get_all_doc_of_student(sid)
+    return HttpResponse(json.dumps(ans, ensure_ascii=False))
