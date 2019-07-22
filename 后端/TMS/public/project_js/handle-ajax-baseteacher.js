2e9670df1c5c80f28e880bf8b0912729de002873
maglith <942399293@qq.com>
2019-07-22 23:53:40 +0800

    JSpost更新
    JSpost更新

--- 后端/TMS/public/project_js/handle-ajax-baseteacher.js
+++ 后端/TMS/public/project_js/handle-ajax-baseteacher.js
@@ -3,7 +3,7 @@ $(function(){
     $.post("/api/get_teachers", {"timestamp": 123}, function(data){
         loadTeacherList(data);
     }, "json");
-    $('#add_list').on("click", addtext_to_list)
+    $('#add_tch').on("click", addTchList)
 })
 
 function loadTeacherList(data){
@@ -14,6 +14,7 @@ function loadTeacherList(data){
         html = html + '<td>' + data[i].id + '</td>';
         html = html + '<td>' + data[i].name + '</td>';
         html = html + '<td>' + data[i].class + '</td>';
+        html = html + '<td>' + data[i].gender + '</td>';
         html = html + '<td><span class="glyphicon glyphicon-trash" onclick="deleteItem(this)"></span></td>';
         html = html + '</tr>';
     }
@@ -23,28 +24,36 @@ function loadTeacherList(data){
 
 function deleteItem(obj){
     $(obj).parent().parent().remove();
+    tch_id = parseInt($(obj).parent().parent().find('td').eq(0).text());
+    $.post("/api/update_user_list", {"timestamp": 123, "method": "del", "user_list": "[" + tch_id + "]"})
 }
 
-function addtext_to_list(){
-    var username = $('#username').val();
-    var guanliclass = $('#guanliclass').val();
-    console.log(username);
-    console.log(guanliclass)
-    if(username != "" && username != null && guanliclass != "" && guanliclass != null ){
-        console.log(username);
-        html = '';
-        html = html + '<tr>';
-        html = html + '<td></td>';
-        html = html + '<td>' + username + '</td>';
-        html = html + '<td>' + guanliclass + '</td>';
-        html = html + '<td><span class="glyphicon glyphicon-trash" onclick="deleteItem(this)"></span></td>';
-        html = html + '</tr>';
-        $('#table_tch_list').append(html);
-        $('#username').val("")
-        $('#guanliclass').val("")
+user_list = []
+function addTchList(){
+    var name = $('#tch_name').val();
+    console.log(typeof(name));
+    var password = $('#tch_password').val();
+    var gender = $('#tch_gender').val();
+    var phone_num = $('#phone_num').val();
+    if (name!="" && name!=null && password!="" && password!=null) {
+        user_list.push('{"name": "' + name + '","password": "' + password + '","gender": "' + gender + '","group":"T", "phone": "' + phone_num + '"}')
+        console.log(user_list)
+        $('#tch_name').val("");
+        $('#tch_password').val("");
+        $('#tch_gender').val("");
+        $('#phone_num').val("");
     } else {
-        alert("请输入完整添加内容")
-        $('#username').val("")
-        $('#guanliclass').val("")
+        alert("请输入完整内容！")
+        $('#tch_name').val("");
+        $('#tch_password').val("");
+        $('#tch_gender').val("");
+        $('#phone_num').val("");
     }
+    $('#confirm_tchlist').click(confirmTchList)
+}
+
+function confirmTchList(){
+    console.log(user_list.toString())
+    $.post("/api/update_user_list", {"timestamp": 123, "method": "add", "user_list": '['+ user_list + ']'});
+    window.location.replace("http://127.0.0.1:8000/admin_teacher/")
 }
