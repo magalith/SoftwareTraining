a384ed22503ecfaa636b70028d60bba0b152f632
maglith <942399293@qq.com>
2019-07-22 23:54:45 +0800

    Revert "JSpost更新"
    This reverts commit 2e9670df1c5c80f28e880bf8b0912729de002873.

--- 后端/TMS/public/project_js/login.js
+++ 后端/TMS/public/project_js/login.js
@@ -4,40 +4,33 @@ $(function () {
     radioClass: 'iradio_square-blue',
     increaseArea: '20%' /* optional */
   });
-  $('.tab-item').click(function(){
-    $(this).addClass("active").siblings("li").removeClass("active");
-    $(".noshow").eq($(this).index()).addClass("selected").siblings("form").removeClass("selected");
-  })
-  $('#userSignIn').on("click", log_in);
-  $('#getCaptcha').click(getCaptcha);
-  $('#captchaSignIn').click(captchaLogin);
+
+  $('#signIn').on("click", log_in)
 });
 
 function log_in() {
     var username = $('#username').val();
     var password = $('#password').val();
     var timestamp = 'dasd';
+    // var href = $(window).attr("href").slice(0, 22);
+    // console.log($(window).attr("href").slice(0, 22));
     if(username != "" && username != null && password != "" && password != null){
         // 执行post指令，向服务器传递用户账号
         $.post("../login", { "user_name": username, "password":password, "timestamp":timestamp }, function(data){
             // console.log(href + data);
             window.location.replace("http://127.0.0.1:8000" + data)
         })
+
+        // function(data){
+        //   alert(data.name); // John
+        //   console.log(data.time); //  2pm
+        
+        console.log(username);
+        console.log(password);
     }else {
         alert("请填写完整")
     }
 }
 
-function getCaptcha() {
-  var phone_num = $('#phone_num').val();
-  $.post("/api/send_sms", {"timestamp": 123, "phone": phone_num})
-}
 
-function captchaLogin(){
-  var phone_num = $('#phone_num').val();
-  var code = $('#captcha').val();
-  $.post("/api/login_phone", {"timestamp": 123, "phone": phone_num, "code": code}, function(data){
-    console.log(data);
-    window.location.replace("http://127.0.0.1:8000" + data)
-  })
-}
+
