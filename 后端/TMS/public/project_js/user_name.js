682405bab9fe7faa95eac24994e467eb99745a5c
Pepper <pepperjao@qq.com>
2019-07-23 10:52:12 +0800

    get_username

--- 后端/TMS/public/project_js/user_name.js
+++ 后端/TMS/public/project_js/user_name.js
@@ -1,11 +1,14 @@
 $(function () {
-
-});
-
-
-function get_username() {
-$.post("/api/get_self_info", {"timestamp":1}, function(data){
-$("#user_name").html("管理员"+ data[0]  +"<small>Member since Nov. 2012</small>+)
+  $.post("/api/get_self_info", {"timestamp":1}, function(data){
+    data = JSON.parse(data);
+    data = data.data
+    get_username(data)
 })
-}
+})
 
+
+function get_username(data) {
+console.log(data)
+$(".user_name").html("管理员:"+ data.name )
+
+}
