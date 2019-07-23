c7f83042c2a6f46a65ea16bf47eb4e63cdb677ea
lichen' job <45004953+lichenrr@users.noreply.github.com>
2019-07-23 20:26:21 +0800

    Update get_class_info.py

--- 后端/TMS/api/lichen/get_class_info.py
+++ 后端/TMS/api/lichen/get_class_info.py
@@ -12,19 +12,19 @@ def class_info():
         # temp_class = Class.objects.filter(id=i.class_id.id)[0]
         # 查询同一班级的学生与学生id
         user_id = User.objects.filter(class_id=i.id)
-        for j in user_id:
-            if (j.group == 'S'):
+        if len(user_id) == True:
+            for j in user_id:
+                if j.group == 'S':
                 # 判断是否为学生
-                stu_list.append(str(j.id))
-            elif(j.group == "T"):
+                    stu_list.append(str(j.id))
+                elif j.group == "T":
                 # 判断是否为老师
-                tea_list.append(str(j.id))
-            else:
-                pass
+                    tea_list.append(str(j.id))
+                else:
+                    pass
 
-        values['class_list'].append({'cid': i.id,
+            values['class_list'].append({'cid': i.id,
                                     'name': i.name,
-                                    'teacher_id': tea_list[0] ,
-                                    'students_id': stu_list,
-        })
+                                    'teacher_id': tea_list,
+                                    'students_id': stu_list,})
     return values
